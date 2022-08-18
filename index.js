9caf0927cb808c2f2aa02b556f8cfe5004059f08
Tricky Licky <wangombevictor5@gmail.com>
2022-08-18 23:21:29 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = "Brain" ;
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage =`WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+const shortGreeting = `Welcome, ${currentUser.toUpperCase()}!`;
+console.log(shortGreeting);
